76d37464e3e159a91a44d9a1040823ba202cc1a4
Pranavi <pranavisrikanth6@gmail.com>
2026-05-19 11:26:16 +1000

    target credit

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -11,16 +11,14 @@ class Target_Follower:
         rospy.init_node('target_follower_node', anonymous=True)
         rospy.on_shutdown(self.clean_shutdown)
 
-        # Because the launch file uses <group ns="$(env VEHICLE_NAME)">,
-        # these topic names must be relative, not /mybota00617/...
         self.cmd_vel_pub = rospy.Publisher(
-            'car_cmd_switch_node/cmd',
+            '/mybota00617/car_cmd_switch_node/cmd',
             Twist2DStamped,
             queue_size=1
         )
 
         rospy.Subscriber(
-            'apriltag_detector_node/detections',
+            '/mybota00617/apriltag_detector_node/detections',
             AprilTagDetectionArray,
             self.tag_callback,
             queue_size=1
@@ -33,7 +31,7 @@ class Target_Follower:
         self.move_robot(msg.detections)
 
     def clean_shutdown(self):
-        rospy.loginfo("System shutting down. Stopping robot...")
+        rospy.loginfo("System shutting down. Stopping robot.")
         self.stop_robot()
 
     def stop_robot(self):
@@ -44,41 +42,44 @@ class Target_Follower:
         self.cmd_vel_pub.publish(cmd_msg)
 
     def move_robot(self, detections):
-        cmd_msg = Twist2DStamped()
-        cmd_msg.header.stamp = rospy.Time.now()
-        cmd_msg.v = 0.0
 
-        # Feature 1: Seek object
-        # If no AprilTag is visible, rotate slowly until one is found.
         if len(detections) == 0:
-            rospy.loginfo("No tag detected - seeking object.")
-            cmd_msg.omega = 1.5
-            self.cmd_vel_pub.publish(cmd_msg)
+            self.stop_robot()
             return
 
-        # Feature 2: Look at object
-        # Use the first detected tag and rotate to keep it centred.
-        tag = detections[0]
+        x = detections[0].transform.translation.x
+        z = detections[0].transform.translation.z
 
-        x = tag.transform.translation.x
-        y = tag.transform.translation.y
-        z = tag.transform.translation.z
+        rospy.loginfo("Tag position x: %.2f, z: %.2f", x, z)
 
-        rospy.loginfo("Tag position x:%f y:%f z:%f", x, y, z)
+        desired_distance = 0.35
 
-        dead_zone = 0.05
-        turn_speed = 1.5
+        distance_error = z - desired_distance
+        center_error = x
 
-        if x > dead_zone:
-            rospy.loginfo("Tag is to the right - turning right.")
-            cmd_msg.omega = -turn_speed
+        kp_linear = 0.6
+        kp_angular = 3.0
 
-        elif x < -dead_zone:
-            rospy.loginfo("Tag is to the left - turning left.")
-            cmd_msg.omega = turn_speed
+        cmd_msg = Twist2DStamped()
+        cmd_msg.header.stamp = rospy.Time.now()
 
-        else:
-            rospy.loginfo("Tag centered - stopping rotation.")
+        cmd_msg.v = kp_linear * distance_error
+        cmd_msg.omega = -kp_angular * center_error
+
+        if cmd_msg.v > 0.25:
+            cmd_msg.v = 0.25
+        elif cmd_msg.v < -0.25:
+            cmd_msg.v = -0.25
+
+        if cmd_msg.omega > 4.0:
+            cmd_msg.omega = 4.0
+        elif cmd_msg.omega < -4.0:
+            cmd_msg.omega = -4.0
+
+        if abs(distance_error) < 0.05:
+            cmd_msg.v = 0.0
+
+        if abs(center_error) < 0.03:
             cmd_msg.omega = 0.0
 
         self.cmd_vel_pub.publish(cmd_msg)
